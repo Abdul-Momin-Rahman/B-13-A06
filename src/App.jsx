20778dc77ecbb966137be36351b79d9dc5c1fd82
Abdul-Momin-Rahman <a.mominatick@gmail.com>
2026-03-30 10:27:21 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+        <h2 className='text-4xl bg-red-500 p-4 text-center'>Hello world How are you</h2>
+
+    </>
+  )
+}
+
+export default App
